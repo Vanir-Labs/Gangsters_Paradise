d2419a29eeffaed3de00e27d610008593cee1138
Vanir-Labs <vanirlabs@gmail.com>
2021-05-17 07:46:20 -0400

    Test

---
+++ Assets/scripts/Player/Player.cs
@@ -0,0 +1,27 @@
+//Player
+//By: Lex King
+//Player Stats, cash, premium states, etc
+// ==================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Health;
+    public float Mana;
+    public double Score;
+    public float Gold;
+    public float Silver;
+    public float Gems;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
